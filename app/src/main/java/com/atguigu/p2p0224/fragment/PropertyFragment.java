4aec010874921f87f680bd29083e637721c9088c
菜鸟大叔 <418593670@qq.com>
2017-06-26 10:52:00 +0800

    资产布局完成

--- app/src/main/java/com/atguigu/p2p0224/fragment/PropertyFragment.java
+++ app/src/main/java/com/atguigu/p2p0224/fragment/PropertyFragment.java
@@ -1,16 +1,21 @@
 package com.atguigu.p2p0224.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.atguigu.p2p0224.R;
 import com.atguigu.p2p0224.base.BaseFragment;
 import com.atguigu.p2p0224.common.AppNetConfig;
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 
 /**
  * Created by Administrator on 2017/6/20.
@@ -18,6 +23,27 @@ import com.atguigu.p2p0224.common.AppNetConfig;
 
 public class PropertyFragment extends BaseFragment {
 
+
+    @Bind(R.id.tv_settings)
+    TextView tvSettings;
+    @Bind(R.id.iv_me_icon)
+    ImageView ivMeIcon;
+    @Bind(R.id.rl_me_icon)
+    RelativeLayout rlMeIcon;
+    @Bind(R.id.tv_me_name)
+    TextView tvMeName;
+    @Bind(R.id.rl_me)
+    RelativeLayout rlMe;
+    @Bind(R.id.recharge)
+    ImageView recharge;
+    @Bind(R.id.withdraw)
+    ImageView withdraw;
+    @Bind(R.id.ll_touzi)
+    TextView llTouzi;
+    @Bind(R.id.ll_touzi_zhiguan)
+    TextView llTouziZhiguan;
+    @Bind(R.id.ll_zichan)
+    TextView llZichan;
 
     @Override
     public String getChildUrl() {
@@ -36,11 +62,15 @@ public class PropertyFragment extends BaseFragment {
 
     @Override
     public void initData() {
-
+        Picasso.with(getActivity())
+                .load(AppNetConfig.BASE_URL+"images/tx.png")
+                .transform(new CropCircleTransformation())
+                .into(ivMeIcon);
     }
 
     @Override
     public int getLayoutId() {
-        return 0;
+        return R.layout.fragment_property;
     }
+
 }
